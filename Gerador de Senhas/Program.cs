707df389ee720f5b5d29befa524c9711d0bc911b
PedroVitorGl <70164809+pedruuv@users.noreply.github.com>
2023-11-21 16:09:39 -0300

    Add files via upload

---
+++ Gerador de Senhas/Program.cs
@@ -0,0 +1,28 @@
+﻿const string CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&(){}/|\\+-=_<>?[]";
+
+void Menu()
+{
+    Console.WriteLine("Digite o tamanho da senha:");
+    var length = Convert.ToInt32(Console.ReadLine());
+
+    var password = GeneratePassword(length);
+    Console.WriteLine(password);
+}
+
+string GeneratePassword(int length)
+{
+    if(length < 8) { length = 8; }
+
+    var password = "";
+
+    for(int i = 0; i < length; i++)
+    {
+        Random random = new();
+        var index = random.Next(0, CHARS.Length - 1);
+        password += CHARS[index];
+    }
+
+    return password;
+}
+
+Menu();
